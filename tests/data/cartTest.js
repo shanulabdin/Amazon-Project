3fc16cf73bae3cd65d5662b5064c7373b4a48d54
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-31 23:54:27 +0500

    Test. the code does nothing if the product is not in the cart, 16l.

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -130,4 +130,23 @@ describe('test suite: updateDeliveryOption', () => {
     expect(cart[0].quantity).toEqual(1);
     expect(cart[0].deliveryOptionId).toEqual('3');
   });
+
+  it('does nothing if the product is not in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    updateDeliveryOption('nothing', '3');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('1');
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  })
 });
